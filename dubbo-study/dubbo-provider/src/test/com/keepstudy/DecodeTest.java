ce9c365a44f2936581b482246bd268630d4ca1d4
xiaokang_glo <xiaokang_glo@qq.com>
2020-03-21 10:31:15 +0800

    dubbo提交代码

--- dubbo-study/dubbo-provider/src/test/com/keepstudy/DecodeTest.java
+++ dubbo-study/dubbo-provider/src/test/com/keepstudy/DecodeTest.java
@@ -11,8 +11,11 @@ import java.net.URLDecoder;
  */
 public class DecodeTest {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String url = "dubbo%3A%2F%2F192.168.193.1%3A20880%2Fcom.keepstudy.api.EmployeeRemoteService%3Fanyhost%3Dtrue%26application%3Ddubbo-provider%26dubbo%3D2.5.5%26generic%3Dfalse%26interface%3Dcom.keepstudy.api.EmployeeRemoteService%26methods%3DgetEmployeeByConditionRemote%26pid%3D3224%26revision%3D1.0-SNAPSHOT%26side%3Dprovider%26timestamp%3D1584711177960";
-        String decode = URLDecoder.decode(url, "UTF-8");
-        System.out.println("解码后："+decode);
+        String urlProvider = "dubbo%3A%2F%2F192.168.193.1%3A20880%2Fcom.keepstudy.api.EmployeeRemoteService%3Fanyhost%3Dtrue%26application%3Ddubbo-provider%26dubbo%3D2.5.5%26generic%3Dfalse%26interface%3Dcom.keepstudy.api.EmployeeRemoteService%26methods%3DgetEmployeeByConditionRemote%26pid%3D3224%26revision%3D1.0-SNAPSHOT%26side%3Dprovider%26timestamp%3D1584711177960";
+        String urlConsumer = "consumer%3A%2F%2F192.168.193.1%2Fcom.keepstudy.api.EmployeeRemoteService%3Fapplication%3Ddubbo-consumer%26category%3Dconsumers%26check%3Dfalse%26dubbo%3D2.5.5%26interface%3Dcom.keepstudy.api.EmployeeRemoteService%26methods%3DgetEmployeeByConditionRemote%26pid%3D11280%26revision%3D1.0-SNAPSHOT%26side%3Dconsumer%26timestamp%3D1584757391336";
+        String decodeProvider = URLDecoder.decode(urlProvider, "UTF-8");
+        String decodeConsumer = URLDecoder.decode(urlConsumer, "UTF-8");
+        System.out.println("provider解码后："+decodeProvider);
+        System.out.println("consumer解码后："+decodeConsumer);
     }
 }
